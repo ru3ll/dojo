c22cbd168a64cd7c1282352ebc936255486de030
= <=>
2024-08-18 22:21:10 +0300

    updated launch sim

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -25,18 +25,6 @@ def generate_launch_description():
                 )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
-    # Include the Gazebo launch file, provided by the gazebo_ros package
-    gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-             )
-
-    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
-    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-                        arguments=['-topic', 'robot_description',
-                                   '-entity', 'syzygy'],
-                        output='screen')
-
     diff_drive_spawner = Node(
         package="controller_manager",
         executable="spawner",
@@ -52,8 +40,6 @@ def generate_launch_description():
     # Launch them all!
     return LaunchDescription([
         rsp,
-        gazebo,
-        spawn_entity,
         diff_drive_spawner,
         joint_broad_spawner
     ])
